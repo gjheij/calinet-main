442e4099fc996191367fa1fa8d849d17b8db9791
Jurjen Heij <jurjenheij@gmail.com>
2026-03-24 17:03:38 +0100

    [MAINT]: formatting

--- scripts/calinet_select.py
+++ scripts/calinet_select.py
@@ -2,9 +2,9 @@
 # emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
 # vi: set ft=python sts=4 ts=4 sw=4 et:
 
-import argparse
-import logging
 import os
+import logging
+import argparse
 
 from calinet.logger import init_logging
 from calinet.exports.selector import (
